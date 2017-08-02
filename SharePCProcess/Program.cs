9649f81fa1a19d34c58afe1b7ba77a801d50cc02
Unknown <razinbuet334@gmail.com>
2017-08-02 16:22:31 +0600

    jkhbi

--- SharePCProcess/Program.cs
+++ SharePCProcess/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Threading;
+
 namespace SharePC
 {
     class Program
@@ -13,27 +15,38 @@ namespace SharePC
            
            
             SharePC pc1 = new SharePC();
+            while (true) {
+                String a1 = Console.ReadLine();
+                if (a1 == "1")
+                {
+                    pc1.triggerResolutionSwitch();
+                    
+                }
+                else {
+                    continue;
+                }
+            }
+           
 
+            //String invitation = pc1.GetControlString();
 
-            String invitation = pc1.GetControlString();
-
-            Console.WriteLine(invitation);
+            //Console.WriteLine(invitation);
             
-            String line1 = Console.ReadLine();
+            //String line1 = Console.ReadLine();
 
-            pc1.destroy();
+            //pc1.destroy();
 
             
-            String invitation2 = pc1.GetControlString();
-            Console.WriteLine(invitation2);
+            //String invitation2 = pc1.GetControlString();
+            //Console.WriteLine(invitation2);
 
         
 
-            String line2 = Console.ReadLine();
+            //String line2 = Console.ReadLine();
 
-            pc1.destroy();
+            //pc1.destroy();
             
-            String line3 = Console.ReadLine();
+            //String line3 = Console.ReadLine();
 
         }
     }
